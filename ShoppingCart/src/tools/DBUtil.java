c6c21e935141f052c016ae0ca82a20ac5e47e334
Classroom <Classroom@192.168.1.34>
2015-07-06 17:04:54 -0400

    added entities from table which brought in customer, order,productinfo and product.jav a classes

--- ShoppingCart/src/tools/DBUtil.java
+++ ShoppingCart/src/tools/DBUtil.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
 
-	public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ELTestProject");
+	public static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ShoppingCart");
 	public static EntityManagerFactory getEmFactory()
 	{
 		return emf;
